11a2c4d00c3e79f23eafdca3c17ba8f10b920843
Md Titumir Anan <titumiranan.gtc@gmail.com>
2025-02-02 22:14:55 +0600

    update

--- src/component/Mywork.tsx
+++ src/component/Mywork.tsx
@@ -6,7 +6,6 @@ import work1 from "./../assets/work1.png";
 import work2 from "./../assets/work2.png";
 import work3 from "./../assets/work3.png";
 import work4 from "./../assets/work4.png";
-// import work5 from "./../assets/work5.png";
 
 import Workcard from "./Workcard";
 import Slider from "react-slick";
